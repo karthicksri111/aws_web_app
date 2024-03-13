3be386d7423718f38fa3c6135bf7649a4f91cf38
Karthik Balanr <Karthik.Balanr@dowjones.com>
2024-03-13 22:15:57 +0530

    Update main.tf

--- base/main.tf
+++ base/main.tf
@@ -1,12 +1,12 @@
 provider "aws" {
   region         = var.region
   assume_role {
-    role_arn     = "arn:aws:iam::${local.account_id}:role/Terraform_Admin_User" 
+    role_arn     = "arn:aws:iam::${local.account_id}:role/Terraform_Role" 
 	}
 }
 
 resource "aws_vpc" "vpc1" {
-  cidr_block       = vars.vpc_cidr
+  cidr_block       = var.vpc_cidr
   instance_tenancy = "default"
   tags = {
     Name = "vpc1"
@@ -15,23 +15,38 @@ resource "aws_vpc" "vpc1" {
 
 resource "aws_internet_gateway" "igw1" {
   vpc_id = aws_vpc.vpc1.id
+    tags = {
+    Name = "igw1"
+  }
 }
 
 resource "aws_subnet" "subnet_a" {
   vpc_id     = aws_vpc.vpc1.id
   cidr_block = var.subnet_a_cidr
+    tags = {
+    Name = "subnet_a"
+  }
 }
 resource "aws_subnet" "subnet_b" {
   vpc_id     = aws_vpc.vpc1.id
   cidr_block = var.subnet_b_cidr
+      tags = {
+    Name = "subnet_b"
+  }
 }
 resource "aws_subnet" "subnet_c" {
   vpc_id     = aws_vpc.vpc1.id
   cidr_block = var.subnet_c_cidr
+      tags = {
+    Name = "subnet_c"
+  }
 }
 resource "aws_subnet" "subnet_d" {
   vpc_id     = aws_vpc.vpc1.id
   cidr_block = var.subnet_d_cidr
+      tags = {
+    Name = "subnet_d"
+  }
 }
 
 resource "aws_route_table" "public" {
@@ -41,16 +56,33 @@ resource "aws_route_table" "public" {
     cidr_block = "0.0.0.0/0"
     gateway_id = aws_internet_gateway.igw1.id
   }
+      tags = {
+    Name = "public"
+  }
 }
-resource "aws_route_table" "private" {
+resource "aws_route_table" "pvt" {
   vpc_id = aws_vpc.vpc1.id
   
   route {
     cidr_block = "0.0.0.0/0"
     gateway_id = aws_nat_gateway.ngw1.id
   }
+      tags = {
+    Name = "pvt"
+  }
+}
+
+resource "aws_eip" "eip1" {
+  domain = "vpc"
+  tags = {
+    Name = "eip1"
+	}
 }
 
 resource "aws_nat_gateway" "ngw1" {
+  allocation_id = aws_eip.eip1.id
   subnet_id = aws_subnet.subnet_b.id
+      tags = {
+    Name = "ngw1"
+  }
 }
