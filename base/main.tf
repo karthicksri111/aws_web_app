c224de7f270a7a24deec4b52ee2ddbc35554d2a3
karthicksri111 <155422099+karthicksri111@users.noreply.github.com>
2024-04-02 14:01:57 +0530

    Update main.tf

--- base/main.tf
+++ base/main.tf
@@ -2,16 +2,16 @@
 module "providers" {
   source = "./modules"
 }
-module "vpc_subnet" {
-  source = "./modules"
-}
-
-module "securiity_groups" {
-  source = "./modules"
-}
-
-module "autoscaling" {
-  source = "./modules"
-}
+#module "vpc_subnet" {
+#  source = "./modules"
+#}
+#
+#module "securiity_groups" {
+#  source = "./modules"
+#}
+#
+#module "autoscaling" {
+#  source = "./modules"
+#}
 
 
